32eaaf0840fa033884e556884efcd2092da19223
Tyler <tyler.a.carey@gmail.com>
2019-12-10 21:40:08 -0500

    upload hw

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,3 @@
-# Modules
 import os
 import csv
 
@@ -6,16 +5,26 @@ import csv
 total_votes = 0
 candidates = []
 candidate_votes = []
-vote_percent = []
 
-# Enter CSV path
+# Define and open CSV path
 csvpath = os.path.join("..", "Resources", "election_data.csv")
+
+
+def poll_data(candidates, candidate_votes, write_csv=False):
+	for i in range(0, len(candidates)):
+		if write_csv == False:
+			print(candidates[i] + ": " + str(round(((candidate_votes[i]/total_votes)*100), 2)) + "% (" + str(candidate_votes[i]) + ")")
+		else:
+			writer.writerow([candidates[i] + ": " + str(round(((candidate_votes[i]/total_votes)*100), 2)) + "% (" + str(candidate_votes[i]) + ")"])
+
 
 with open(csvpath, newline="") as election_data:
 	election = csv.reader(election_data, delimiter=",")
 	next (election_data)
 
 	for row in election:
+		
+		# Adds to total votes for each row
 		total_votes += 1
 		
 		# Checks each candidate against the initialized list
@@ -30,36 +39,27 @@ with open(csvpath, newline="") as election_data:
 			if row[2] == candidates[i]:
 				candidate_votes[i] += 1
 
-				
 
-print("Election Results")
-print("---------------------------")
-print("Total Votes: " + str(total_votes))
-print("---------------------------")
+winner = candidates[candidate_votes.index(max(candidate_votes))]
 
-# Calculates vote percent for each candidate. It's ugly but it works.
-for i in range(0, len(candidates)):
-	print(candidates[i] + ": " + str(round(((candidate_votes[i]/total_votes)*100), 2)) + "% (" + str(candidate_votes[i]) + ")")
-print("---------------------------")
+output_string = [
+	"Election Results\n---------------------------\nTotal Votes: " + 
+	str(total_votes) + "\n---------------------------",
+	"---------------------------\nWinner: " + winner +
+	"\n---------------------------"
+	]
+
+
+print(output_string[0])
+poll_data(candidates, candidate_votes)
+print(output_string[1])
 
 # Uses the index of the largest value in the votes list to determine the winner
-print("Winner: " + candidates[candidate_votes.index(max(candidate_votes))])
-print("---------------------------")
 
 output_file = os.path.join("..", "Output", "Poll Analysis.txt")
 
-with open(output_file, "w", newline='') as datafile:
+with open(output_file, 'w', newline='') as datafile:
 	writer = csv.writer(datafile)
-	writer.writerow(["Election Results"])
-	writer.writerow(["---------------------------"])
-	writer.writerow(["Total Votes: " + str(total_votes)])
-	writer.writerow(["---------------------------"])
-	for i in range(0, len(candidates)):
-		writer.writerow([candidates[i] + ": " + str(round(((candidate_votes[i]/total_votes)*100), 2)) + "% (" + str(candidate_votes[i]) + ")"])
-	writer.writerow(["---------------------------"])
-	writer.writerow(["Winner: " + candidates[candidate_votes.index(max(candidate_votes))]])
-	writer.writerow(["---------------------------"])
-	
-	
-	
-	
+	writer.writerow([output_string[0]])
+	poll_data(candidates, candidate_votes, True)
+	writer.writerow([output_string[1]])
